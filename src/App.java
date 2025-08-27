23903d07929ddb567c0148d8aeb7c40e7b9b218b
Teesta-voya <Teesta.Majumder@voyaindia.com>
2025-08-27 19:46:29 +0000

    working with Array data

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 public class App {
   
   public static void main(String[] args) {
+    double[] lotteryums = {23,45,78,34,98};
+    System.out.println(lotteryums[2]);
+    lotteryums[2]=67;
+    System.out.println(lotteryums[2]);
+
     
   }
 }
