e37edbaea59fcb5bd40c6652df388c5845643436
Teesta-voya <Teesta.Majumder@voyaindia.com>
2025-08-28 07:30:57 +0000

    iteration through java

--- src/App.java
+++ src/App.java
@@ -1,10 +1,30 @@
+
+import java.util.Arrays;
+
 public class App {
   
   public static void main(String[] args) {
-    double[] lotteryums = {23,45,78,34,98};
-    System.out.println(lotteryums[2]);
-    lotteryums[2]=67;
-    System.out.println(lotteryums[2]);
+    int arr[] = {23,45,78,34,98};
+   // System.out.println(lotteryums[2]);
+   // lotteryums[2]=67;
+    //System.out.println(lotteryums[2]);
+    for(int i=0;i<arr.length;i++)
+    {
+    System.out.println(arr[i]);
+    }
+    System.out.println();
+
+    for(int i=arr.length-1;i>=0;i--)
+    {
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+    for(int item:arr)
+    {
+      System.out.println(item);
+    }
+    System.out.println();
+    Arrays.stream(arr).forEach(System.out::println);
 
     
   }
